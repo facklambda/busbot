2f9b3a99cb91103b4fbbbf8b2bc7d4d723bef8f8
fack <4878248+facktheplanet@users.noreply.github.com>
2021-11-16 22:46:22 -0600

    code runs now lol

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,33 @@ use reqwest::Error;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct User {
-    login: String,
-    id: u32,
+struct Nexttripdeparture {
+    Actual: bool,
+    BlockNumber: u32,
+    DepartureText: String,
+    DepartureTime: String,
+    Description: String,
+    Gate: Option<String>,
+    Route: String,
+    RouteDirection: String,
+    Terminal: String,
+    VehicleHeading: u32,
+    VehicleLatitude: u32,
+    VehicleLongitude: u32,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let request_url = format!(
         "https://svc.metrotransit.org/NexTrip/{stopid}?format=json",
-        stopid = "15280"
+        stopid = "15280" //15412 eastbound
     );
     println!("{}", request_url);
     let response = reqwest::get(&request_url).await?;
+    
+    println!("{:#?}", response);
 
-    let nexttrips: Vec<Nexttrips> = response.json().await?;
-    println!("{:?}", nexttrips);
+    let departures: Vec<Nexttripdeparture> = response.json().await?;
+    println!("{:?}", departures);
     Ok(())
 }
